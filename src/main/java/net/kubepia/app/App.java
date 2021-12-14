d70624edbbe36fafcbf015aa33653395dcd855e6
nogada <seungil.han@gmai.com>
2021-12-14 14:47:23 +0900

    singleoine log

--- src/main/java/net/kubepia/app/App.java
+++ src/main/java/net/kubepia/app/App.java
@@ -13,11 +13,11 @@ public class App
 
     public static void main( String[] args )
     {
-        String msg = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n It has survived not only five centuries, but also the leap into electronic typesetting, \nremaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets \ncontaining Lorem Ipsum passages, and more recently with desktop publishing software like Aldus \nPageMaker including versions of Lorem Ipsum.";
+        String msg = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
         Map<String, String> env = System.getenv();
         int count = Integer.parseInt(env.get("RUN_COUNT"));
         int wait = Integer.parseInt(env.get("RUN_SLEEP"));
-
+        
         for(int i=0;i<count;i++){
             try {
                 TimeUnit.MILLISECONDS.sleep(wait);
